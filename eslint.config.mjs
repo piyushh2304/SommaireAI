5940e8bea38b43bc32ab3bfa95d6bb940e6b80ba
Piyush Rajput <118372177+piyushh2304@users.noreply.github.com>
2025-07-09 20:23:12 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,7 +12,6 @@ export default [
     "next/core-web-vitals",
     "next/typescript"
   ),
-  eslintPluginPrettierRecommended,
   {
     rules: {
       "react/no-escaped-entities": "off",
