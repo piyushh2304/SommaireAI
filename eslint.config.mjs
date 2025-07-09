fb99924bf7b95847863bc54e9da7d25972910f1b
Piyush Rajput <118372177+piyushh2304@users.noreply.github.com>
2025-07-09 20:39:32 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,5 +10,8 @@ const compat = new FlatCompat({ baseDirectory: __dirname });
 export default [
    ...compat.config({
     extends: ['next/core-web-vitals', 'next/typescript'],
-  }),
+  } rules: {
+      "react/no-escaped-entities": "off",
+    },
+   ),
 ];
